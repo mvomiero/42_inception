2b052a82b908aa29636435c43d8108b9b238cdef
Marco Vomiero <colocomarco1@gmail.com>
2023-07-23 13:41:42 +0200

    first

---
+++ srcs/containers/nginx/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My PHP Page</title>
+</head>
+<body>
+    <?php
+        echo "<h1>Hello, Docker!</h1>";
+        echo "<p>This is a simple PHP page served by Nginx in a Docker container.</p>";
+    ?>
+</body>
+</html>
