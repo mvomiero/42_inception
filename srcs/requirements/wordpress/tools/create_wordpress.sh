d1355d8105147ed29b192141c5ee71e6bf90ecb7
Marco Vomiero <colocomarco1@gmail.com>
2023-07-26 00:06:09 +0200

    works for now

--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -8,6 +8,8 @@ else
 
 ####### MANDATORY PART ##########
 
+####### MANDATORY PART ##########
+
 	#Download wordpress and all config file
 	wget http://wordpress.org/latest.tar.gz
 	tar xfz latest.tar.gz
@@ -16,11 +18,12 @@ else
 	rm -rf wordpress
 
 	#Inport env variables in the config file
-#	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-#	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-#	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-#	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-#	cp wp-config-sample.php wp-config.php
+	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
+	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
+	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
+	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
+	cp wp-config-sample.php wp-config.php
+###################################
 
 fi
 
