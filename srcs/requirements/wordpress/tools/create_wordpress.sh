33e38c1bafb1143917a631bf0de2b8e27d77e7c4
mvomiero <colocomarco1@gmail.com>
2023-07-26 10:05:54 -0400

    bullseye wp updated

--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -1,29 +1,24 @@
 #!/bin/sh
 
-#check if wp-config.php exist
+# Check if wp-config.php exists
 if [ -f ./wp-config.php ]
 then
-	echo "wordpress already downloaded"
+    echo "WordPress already downloaded"
 else
 
-####### MANDATORY PART ##########
+    # Download WordPress and all config files
+    wget http://wordpress.org/latest.tar.gz
+    tar xfz latest.tar.gz
+    mv wordpress/* .
+    rm -rf latest.tar.gz
+    rm -rf wordpress
 
-####### MANDATORY PART ##########
-
-	#Download wordpress and all config file
-	wget http://wordpress.org/latest.tar.gz
-	tar xfz latest.tar.gz
-	mv wordpress/* .
-	rm -rf latest.tar.gz
-	rm -rf wordpress
-
-	#Inport env variables in the config file
-	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-	cp wp-config-sample.php wp-config.php
-###################################
+    # Import env variables in the config file
+    sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
+    sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
+    sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
+    sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
+    cp wp-config-sample.php wp-config.php
 
 fi
 
