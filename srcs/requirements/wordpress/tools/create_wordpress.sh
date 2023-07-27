59ddfed5d5179901d47d81807c21cf7206426537
mvomiero <colocomarco1@gmail.com>
2023-07-27 07:44:58 -0400

    update users

--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -14,8 +14,8 @@ else
     rm -rf wordpress
 
     # Import env variables in the config file
-    sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-    sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
+    sed -i "s/username_here/$WP_ADMIN/g" wp-config-sample.php
+    sed -i "s/password_here/$WP_ADMIN_PASSWORD/g" wp-config-sample.php
     sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
     sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
     cp wp-config-sample.php wp-config.php
